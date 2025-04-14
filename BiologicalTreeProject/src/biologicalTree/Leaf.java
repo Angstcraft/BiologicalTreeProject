a03d92d351f6b9656965f54d63cba62f7265925b
Angstcraft <70594879+Angstcraft@users.noreply.github.com>
2025-04-14 11:56:23 +0000

    Add files via upload

---
+++ BiologicalTreeProject/src/biologicalTree/Leaf.java
@@ -0,0 +1,47 @@
+package biologicalTree;
+
+class Leaf
+{
+   private String type;
+   private double height;
+   private double width;
+
+   /**
+    * 
+    * @param pType
+    */
+   public Leaf(String pType)
+   {
+	  this.type = pType;
+	  this.height = 5.0; // Default height
+	  this.width = 2.0; // Default width
+   }
+
+
+   @Override
+   public boolean equals(Object obj)
+   {
+	  if (this == obj)
+		 return true;
+	  if (obj == null || getClass() != obj.getClass())
+		 return false;
+	  Leaf leaf = (Leaf) obj;
+	  return Double.compare(leaf.height, height) == 0 && Double.compare(leaf.width, width) == 0
+			   && type.equals(leaf.type);
+   }
+   public String getType()
+   {
+	  return type;
+   }
+
+   public double getHeight()
+   {
+	  return height;
+   }
+
+   public double getWidth()
+   {
+	  return width;
+   }
+
+}
